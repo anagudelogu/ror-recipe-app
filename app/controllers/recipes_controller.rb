3e31ab5fae174f17439f6fbe42d447aac98d6a87
Andres Agudelo <anagudelogu@gmail.com>
2022-08-30 13:45:28 -0500

    Implement recipes list functionality:
    - Listing current_user recipes.
    - Show names, descriptions and delete buttons of all recipes.
    - Add button to create new recipes.
    - Clicking a recipe leads to its details.
    - Clicking on DELETE button destroys the recipe.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,16 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = Recipe.where(user: current_user)
+  end
+
+  def show; end
+
+  def new; end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    redirect_to recipes_path
+  end
 end
