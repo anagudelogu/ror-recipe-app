9fd63f5ca6c8dc841521dfabc7070adc03e997c1
Andres Agudelo <anagudelogu@gmail.com>
2022-08-31 06:02:30 -0500

    Implement action to create a new Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,12 +5,31 @@ class RecipesController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe = current_user.recipes.build(recipe_params)
+
+    if recipe.save
+      redirect_to recipes_path, notice: "Succesfully created recipe #{recipe.name}"
+    else
+      flash.now[:alert] = recipe.errors.full_messages.to_sentence
+      render :new
+    end
+  end
 
   def destroy
     @recipe = Recipe.find(params[:id])
     @recipe.destroy
 
     redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
