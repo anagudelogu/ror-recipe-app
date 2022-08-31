b2efbf721ea460540deeb07bce2dda32e12450c0
marurevi <maryrev@hotmail.com>
2022-08-31 12:50:30 -0300

    Create foods new view, modify controller and implement test

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,33 @@
 class FoodsController < ApplicationController
-  def index; end
+  def new
+    @user = current_user
+    @food = Food.new
+  end
+
+  def create
+    @food = current_user.foods.new(food_params)
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @foods = current_user.foods
+  end
+
+  def show; end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
