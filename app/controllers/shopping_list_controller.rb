877cc624b80b2fbd0ce75bb5c098fe6198ed0b2e
marurevi <maryrev@hotmail.com>
2022-09-01 21:21:03 -0300

    Implement desired functionality for shopping list

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,3 +1,11 @@
 class ShoppingListController < ApplicationController
-  def index; end
+  def index
+    @inventory = current_user.inventory
+    @ingredients = current_user.recipes.map(&:ingredients).reduce { |a, b| a.merge(b) { |_, m, n| m + n } } || {}
+    @shopping_list = @ingredients.merge(@inventory.select { |k, _v| @ingredients.key?(k) }) do |_, c, d|
+      { qt: c - d[:qt], price: d[:price] } if (c - d[:qt]).positive?
+    end
+
+    @shopping_list = @shopping_list.compact
+  end
 end
